28dca1ab125d4689df7bf6383d6f1229dbbc8f13
jokerxin <1529177144@qq.com>
2021-07-21 22:54:21 +0800

    Test the JSON data returned by AJAX

--- AJAX/js/server.js
+++ AJAX/js/server.js
@@ -8,6 +8,11 @@ app.get("/server", (request, response) => {
     response.send("hello world");
 });
 
+app.get("/json-server", (request, response) => {
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.send(JSON.stringify({ name: "jokerxin" }));
+});
+
 app.listen(8000, () => {
     console.log("服务已启动，8000端口监听中 ...");
 });
