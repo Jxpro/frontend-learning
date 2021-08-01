b2a54f27abdd3ca676281692fdb84129889e4541
jokerxin <1529177144@qq.com>
2021-08-01 17:24:00 +0800

    Test the document.write method

---
+++ js/document_write.js
@@ -0,0 +1,9 @@
+document.write(555);
+
+window.onload = function () {
+    document.write(666);
+    document.write(777);
+    // document.close();
+    document.open();
+    document.write(222);
+};
